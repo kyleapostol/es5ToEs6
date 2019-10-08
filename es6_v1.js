de652f1ecc620f24ba6c510a69b820afc7fea790
Kyle Apostol <kyleapostol@yahoo.com>
2019-10-08 22:14:48 +0000

    es5 to es6

--- es6_v1.js
+++ es6_v1.js
@@ -1,7 +1,3 @@
-
-
-
-
 var animals = [
 	{ type: 'giraffe', height: 8.2, weight: 315, name: 'Geofrie', top3Foods: ['leaves','roots','children']},
 	{ type: 'tiger', height: 2.1, weight: 120, name: 'Tony', top3Foods: ['meat','children','corn flakes']}
@@ -9,26 +5,19 @@ var animals = [
 
 animalListing(animals, true);
 
-
 function animalListing( animalArray, listFoods){
 	if(listFoods===undefined){
 		listFoods = true;
 	}
 	var outputArray = [];
-	for(var animalIndex = 0; animalIndex< animalArray.length; animalIndex++){
-		var thisAnimal = animalArray[animalIndex];
-		var name = thisAnimal.name;
-		var height = thisAnimal.height;
-		var weight = thisAnimal.weight;
-		var type = thisAnimal.type;
-		var foods = thisAnimal.top3Foods;
-		var food1 = foods[0];
-		var food2 = foods[1];
-		var food3 = foods[2];
-		var sentence = "Animal: "+name+" is a "+type+" and is "+height+" meters at "+(weight*2.2)+" lbs";
+	for(let animalIndex = 0; animalIndex< animalArray.length; animalIndex++){  
+        const {name, age, weight, type, top3Foods:foods} =animalArray[animalIndex];
+        const [food1, food2, food3] = foods;
+        let sentence = "Animal: "${name}" is a "${type}" and is "${height}" meters at "${weight*2.2}" lbs";
 		console.log(sentence);
-		if(listFoods){
-			sentence += " and it likes to eat "+food1+", "+food2+", and "+food3;
+		
+        if(listFoods){
+			sentence += " and it likes to eat "${food1}", "${food2}", and "+${food3};
 			console.log(foodSentence)
 		}
 	}
